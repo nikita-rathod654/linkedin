14f5368d5174d3971d8c16e567e02a5585100002
NikitaRathod <nr0005650@gmail.com>
2025-09-30 12:54:42 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: "https://linkedin-dl5a.onrender.com",
+	baseURL: "https://linkedin-dl5a.onrender.com/api/v1",
 	withCredentials: true,
 });
